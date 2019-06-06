db276570a1c84b9e168ceeada80daa0007005a7e
mikehuangxp <up758038@myport.ac.uk>
2019-06-06 22:27:07 +0100

    Commit 3

--- Chapter 2/RE matches IP address.py
+++ Chapter 2/RE matches IP address.py
@@ -6,4 +6,3 @@ result = re.match('(\w+\-\w+\.\d+)\s+(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})\s+(\w+)\s
 print('-'*80)
 print('%-7s : %s' % ('接口',result[0]))
 print('%-7s : %s' % ('IP地址',result[1]))
-print('%-7s : %s' % ('状态',result[4]))
