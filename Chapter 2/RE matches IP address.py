628f9fe148f39895d9fb8b8e779dca7ceeef8eeb
mikehuangxp <up758038@myport.ac.uk>
2019-06-06 22:21:43 +0100

    Commit 2

--- Chapter 2/RE matches IP address.py
+++ Chapter 2/RE matches IP address.py
@@ -1,4 +1,4 @@
-str1 = 'Port-channel1.189      192.168.189.254      YES      CONFIG    UP      UP    '
+str1 = 'Port-channel1.189      192.168.189.254      YES      CONFIG    UP     '
 import re
 
 result = re.match('(\w+\-\w+\.\d+)\s+(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})\s+(\w+)\s+(\w+)\s+(\w+)\s+(\w+)\s+',str1).groups()
@@ -6,4 +6,4 @@ result = re.match('(\w+\-\w+\.\d+)\s+(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})\s+(\w+)\s
 print('-'*80)
 print('%-7s : %s' % ('接口',result[0]))
 print('%-7s : %s' % ('IP地址',result[1]))
-print('%-7s : %s' % ('状态',result[5]))
+print('%-7s : %s' % ('状态',result[4]))
